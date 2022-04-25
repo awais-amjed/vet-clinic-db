8405e5e7ad43c8622419f6fec0c556be0eedca03
Awais Amjed <awaisamjed171@gmail.com>
2022-04-25 17:27:49 +0500

    Add schema query to add animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    date_of_birth DATE,
+    neutered BIT,
+    weight_kg DECIMAL,
+    PRIMARY KEY(id)
 );
