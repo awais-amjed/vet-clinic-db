4341530824613b5d3aa2f9c70a36dee4b485b623
Awais Amjed <awaisamjed171@gmail.com>
2022-04-27 14:59:15 +0500

    Update schema.sql to include Foreign keys

--- schema.sql
+++ schema.sql
@@ -7,19 +7,26 @@ CREATE TABLE animals (
     escape_attempts INT,
     neutered BOOL,
     weight_kg DECIMAL,
-    species varchar(200),
-    PRIMARY KEY(id)
+    species_id INT,
+    owner_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_species
+        FOREIGN KEY (species_id)
+            REFERENCES species(id),
+    CONSTRAINT fk_owners
+        FOREIGN KEY (owner_id)
+            REFERENCES owners(id)
 );
 
 CREATE TABLE owners (
     id INT GENERATED ALWAYS AS IDENTITY,
     full_name VARCHAR(250),
     age INT,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE species (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(250),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
