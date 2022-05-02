82d9b64b5c519acc18b2435e360dc595ecd8a54a
Awais Amjed <awaisamjed171@gmail.com>
2022-05-02 15:26:02 +0500

    Add explain analyze queries & their improvements

--- queries.sql
+++ queries.sql
@@ -105,3 +105,13 @@ JOIN (
     WHERE v.name = 'Maisy Smith'
     GROUP BY a.species_id ORDER BY visit_count DESC LIMIT 1
 ) as sub_query on s.id = sub_query.species_id;
+
+-- Explain analyze queries
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT * FROM visits where vet_id = 2;
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+-- Improvements
+explain analyze SELECT COUNT(1) FROM visits where animal_id = 4; -- Use count(1) instead of count(*)
+CREATE INDEX vet_id_asc ON visits(vet_id ASC);
+CREATE INDEX email_asc ON owners(email ASC);
